454c22ea3df6fceb8f5088b623d33654c5d28687
Jonas Russer <jonas.russer@gmail.com>
2019-07-31 17:20:52 +0200

    GUI finished

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
 import tkinter
 import request
+import os
+from PIL import Image,ImageTk
+
 
 HEIGHT = 600
 WIDTH = 700
 
 #Main Window
 root = tkinter.Tk()
+root.geometry(str(WIDTH) + "x" + str(HEIGHT))
 
-canvas = tkinter.Canvas(root,bg='white',height=HEIGHT, width=WIDTH)
+image = Image.open("bg.jpg")
+photo = ImageTk.PhotoImage(image)
 
-top_frame = tkinter.Frame(root,bg='blue',bd=3)
-bottom_frame = tkinter.Frame(root,bg='blue',bd=5)
+bg_label = tkinter.Label(root,image=photo)
+
+top_frame = tkinter.Frame(root,bg='#05d5ec',bd=3)
+bottom_frame = tkinter.Frame(root,bg='#05d5ec',bd=5)
 
 button = tkinter.Button(top_frame, text='Get Weather')
 textfield = tkinter.Entry(top_frame)
@@ -20,7 +27,8 @@ label = tkinter.Label(bottom_frame)
 
 
 #Adding all of my shit to root 
-canvas.pack()
+
+bg_label.place(relwidth=1,relheight=1)
 top_frame.place(relx= 0.15,rely=0.1,relheight=0.05,relwidth=0.7)
 bottom_frame.place(relx = 0.15,rely=0.25,relheight=0.6,relwidth=0.7)
 button.place(relx=0.75,relwidth=0.25,relheight=1)
