38ae230ec7ba2a509e511ca8baec1fd0138ba4d5
Jonas Russer <jonas.russer@gmail.com>
2019-07-31 17:45:20 +0200

    Working API Call

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
 import tkinter
-import request
+import configparser
+import requests
 import os
 from PIL import Image,ImageTk
 
+
+def get_api_config():
+    config= configparser.ConfigParser()
+    config.read('config.ini')
+    return config['openweathermap']['api']
+
+def get_weather(city):
+    api_key = get_api_config()
+    url = "http://api.openweathermap.org/data/2.5/weather?q={},de&APPID={}".format(city,api_key)
+    req = requests.get(url)
+    data = req.json()
+    print(data)
+    return data
 
 HEIGHT = 600
 WIDTH = 700
 
 #Main Window
 root = tkinter.Tk()
+root.title("Weather App")
 root.geometry(str(WIDTH) + "x" + str(HEIGHT))
 
 image = Image.open("bg.jpg")
@@ -19,8 +34,9 @@ bg_label = tkinter.Label(root,image=photo)
 top_frame = tkinter.Frame(root,bg='#05d5ec',bd=3)
 bottom_frame = tkinter.Frame(root,bg='#05d5ec',bd=5)
 
-button = tkinter.Button(top_frame, text='Get Weather')
 textfield = tkinter.Entry(top_frame)
+button = tkinter.Button(top_frame, text='Get Weather',command= lambda: get_weather(textfield.get()))
+
 
 label = tkinter.Label(bottom_frame)
 
